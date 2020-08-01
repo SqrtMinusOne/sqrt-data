cb3f2c2f0bf8c288b9c56bdc20a89f88d211c4d5
SqrtMinusOne <thexcloud@gmail.com>
2020-08-01 16:36:43 +0300

    Parse MPD watcher log

---
+++ models/SongListened.py
@@ -0,0 +1,21 @@
+import sqlalchemy as sa
+from .base import Base
+
+__all__ = ['SongListened']
+
+class SongListened(Base):
+    __tablename__ = 'SongListened'
+    __table_args__ = {'schema': 'mpd'}
+
+    song_id = sa.Column(
+        sa.BigInteger(),
+        sa.ForeignKey('mpd.MpdSong.id'),
+        primary_key=True,
+        nullable=False
+    )
+    
+    time = sa.Column(
+        sa.DateTime(),
+        nullable=False,
+        primary_key=True
+    )
--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .base import *
 from .MpdSong import *
+from .SongListened import *
 from .VkMessage import *
 from .VkUser import *
