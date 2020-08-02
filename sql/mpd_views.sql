ba4ddf6f2c75a907ff44d51035365266c5e2ed77
SqrtMinusOne <thexcloud@gmail.com>
2020-08-02 11:57:10 +0300

    Stuff

---
+++ sql/mpd_views.sql
@@ -0,0 +1,12 @@
+drop view if exists mpd."MpdSongListened";
+create view mpd."MpdSongListened" as
+select
+    S.title title,
+    S.album album,
+    S.album_artist artist,
+    S.duration::float4 / 60 duration,
+    S.year "year",
+    L.time "time"
+from mpd."SongListened" L
+left join mpd."MpdSong" S ON L.song_id = S.id
+order by time asc
