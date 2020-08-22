cbdb202de4b25719d0e5c12ea07421e44782e078
SqrtMinusOne <thexcloud@gmail.com>
2020-08-22 22:24:37 +0500

    logging

--- cli.py
+++ cli.py
@@ -1,9 +1,16 @@
 import click
 import inquirer
+import logging
 
 from api import get_filenames, hash_set, list_hashes
 from cli import mpd
 
+logging.basicConfig(
+    filename='./cli.log',
+    level=logging.DEBUG,
+    format='%(asctime)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 @click.group()
 def cli():
@@ -21,6 +28,7 @@ def hash_list():
 @cli.command()
 @click.option('-n', '--name', required=False, type=str)
 def hash_toggle(name):
+    logging.info('Toggled hash for %s', name)
     if name is None:
         name = inquirer.prompt(
             [inquirer.List('filename', 'Select filename',
