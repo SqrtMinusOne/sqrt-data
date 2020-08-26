83a0a2276543eaeb34d6727ac2c8b8e2e60f967a
SqrtMinusOne <thexcloud@gmail.com>
2020-08-26 15:38:38 +0300

    aw to-csv in sync-logs.sh

--- scripts/sync-logs.sh
+++ scripts/sync-logs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+PYTHON="/home/pavel/Programs/miniconda3/bin/python"
+CLI="/home/pavel/Code/Data/cli.py"
 DATA="$(hostname): $(date +"%Y-%m-%d")"
 LOG_FILE="/home/pavel/logs-sync/sync.log"
 if grep -Fq "$DATA" $LOG_FILE; then
     echo "Already synced today";
 else
+    $PYTHON $CLI aw to-csv
     export RSYNC_EXCLUDE_PATTERN="sync.log"
     export CREATE_DIRS=yes
     /usr/local/bin/osync.sh --initiator=/home/pavel/logs-sync --target=ssh://pavel@45.76.36.229//home/pavel/logs-sync || exit 1
