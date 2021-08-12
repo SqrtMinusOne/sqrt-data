ba37a2de4945a3022b6ada8497a7935090060f87
SqrtMinusOne <thexcloud@gmail.com>
2021-08-12 21:49:29 +0300

    feat(mpd): ignore not found for now

--- sqrt_data/parse/mpd/load_logs.py
+++ sqrt_data/parse/mpd/load_logs.py
@@ -22,15 +22,21 @@ def put_log(filename):
     logging.info('Reading %s', filename)
     df = pd.read_csv(filename)
     records = df.to_dict(orient='records')
+    all_found = True
     with DBConn.get_session() as db:
         for record in tqdm(records):
             if record['type'] == 'skipped':
                 continue
             song = db.query(MpdSong).filter_by(file=record['file']).first()
-            listened = SongListened(song_id=song.id, time=record['time'])
-            db.merge(listened)
+            if song:
+                listened = SongListened(song_id=song.id, time=record['time'])
+                db.merge(listened)
+            else:
+                logging.error('Song %s not found', record['file'])
+                all_found = False
         db.commit()
-    save_hash(filename)
+    if all_found:
+        save_hash(filename)
 
 
 def load_logs():
