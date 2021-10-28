73771b7927846555baf40eb86ad1a912cf7c8112
SqrtMinusOne <thexcloud@gmail.com>
2021-10-28 20:32:50 +0300

    feat(*): literate first version

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pandas', 'numpy', 'click', 'inquirer', 'python-mpd2', 'sqlalchemy',
-        'psycopg2-binary', 'requests', 'tqdm', 'beautifulsoup4'
+        'psycopg2-binary', 'requests', 'tqdm', 'beautifulsoup4', 'dynaconf',
+        'sqlitedict'
     ],
     entry_points='''
     [console_scripts]
