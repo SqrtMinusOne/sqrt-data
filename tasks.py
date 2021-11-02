8242acd98af3ea5cb9191a4bb06c1b16d264c806
SqrtMinusOne <thexcloud@gmail.com>
2021-11-02 19:59:54 +0300

    feat(*): use python schedule instead of mcron

---
+++ tasks.py
@@ -0,0 +1,40 @@
+# [[file:org/index.org::*Docker][Docker:1]]
+import time
+import schedule
+import subprocess
+
+
+def waka_task():
+    p = subprocess.run(['sqrt_data', 'waka', 'get_data'])
+    if p.returncode != 0:
+        return
+    subprocess.run(['sqrt_data', 'waka', 'load'])
+
+
+def mpd_task():
+    p = subprocess.run(['sqrt_data', 'mpd', 'load_library'])
+    if p.returncode != 0:
+        return
+    subprocess.run(['sqrt_data', 'mpd', 'load_logs'])
+
+
+def sleep_task():
+    subprocess.run(['sqrt_data', 'sleep', 'load'])
+
+
+def aw_task():
+    p = subprocess.run(['sqrt_data', 'aw', 'load'])
+    if p.returncode != 0:
+        return
+    suprocess.run('sqrt_data', 'aw', 'preprocessing_dispatch')
+
+
+schedule.every().day.at('00:00').do(waka_task)
+schedule.every().day.at('01:00').do(mpd_task)
+schedule.every().day.at('02:00').do(sleep_task)
+schedule.every().day.at('03:00').do(aw_task)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+# Docker:1 ends here
