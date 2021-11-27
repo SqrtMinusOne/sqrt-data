1423ba6fd77eb8695754c01dd59ff45e508d0991
SqrtMinusOne <thexcloud@gmail.com>
2021-11-27 21:17:24 +0300

    feat(aw): aw app conversion & browser post-processing

--- sqrt_data/parse/aw/postprocess.py
+++ sqrt_data/parse/aw/postprocess.py
@@ -135,9 +135,11 @@ begin
                          W.timestamp + interval '1 minute') -
                    greatest(W.timestamp, T.timestamp)) duration,
            T.url,
+           T.site,
+           T.url_no_params,
            T.title,
            T.audible,
-           T.tab_count
+           T.tab_count,
     FROM T
              INNER JOIN W ON
         ((W.timestamp, W.timestamp + interval '1 minute')
